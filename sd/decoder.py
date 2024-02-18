2f62ff266fd503cb1119e9892460ab1d00f9487f
Thomas Bocandé <thomasbocande@gmail.com>
2024-02-18 01:02:17 +0100

    end decoder

--- sd/decoder.py
+++ sd/decoder.py
@@ -3,11 +3,36 @@ from torch import nn
 from torch.nn import functional as F
 from attention import SelfAttention
 
-class VAE_ResidualBlock(nn.Module):
+class VAE_AttentionBlock(nn.Module):
     
     def __init__(self, channels: int):
         super().__init__()
         self.groupnorm = nn.GroupNorm(32, channels)
+        self.attention = SelfAttention(1, channels)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (Batch_Size, Features, Height, Width)
+        
+        residue = x
+        
+        n, c, h, w = x.shape()
+        
+        # (Batch_Size, Features, Height, Width) -> (Batch_Size, Features, Height * Width)
+        x = x.view(n, c, h * w)
+        
+        # (Batch_Size, Features, Height * Width) -> (Batch_Size, Height * Width, Features)
+        x = x.transpose(-1, -2)
+        
+        # (Batch_Size, Height * Width, Features) -> (Batch_Size, Height * Width, Features)
+        x = self.attention(x)
+        
+        # (Batch_Size, Height * Width, Features) -> (Batch_Size, Features, Height * Width)
+        x = x.transpose(-1, -2)
+        
+        # (Batch_Size, Features, Height * Width) -> (Batch_Size, Features, Height, Width)
+        x = x.view((n, c, h, w))
+        
+        return x + residue
 
 class VAE_ResidualBlock(nn.Module):
     
@@ -25,7 +50,7 @@ class VAE_ResidualBlock(nn.Module):
             self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0)
         
     def foward(self, x: torch.Tensor) -> torch.Tensor:
-        # x: (Batch_Size, In_Channel, Heigh, Width)
+        # x: (Batch_Size, In_Channel, Height, Width)
         
         residue = x
         
